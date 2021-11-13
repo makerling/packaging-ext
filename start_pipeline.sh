549df021d4a07aa80783c9dbf6f2d47489dc82d1
Stevan Vanderwerf <github@vanderling.com>
2021-11-13 10:26:18 -0600

    printing response of curl command

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -2,9 +2,11 @@
 
 source .env
 
-curl -H "Accept: application/vnd.github.everest-preview+json" \
+response=$(curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token "$token"" \
     --request POST \
     --data '{"event_type": "create-deb-rpm-package"}' \
     https://api.github.com/repos/makerling/packaging-ext/dispatches \
-    --write-out "%{http_code}\n"
+    --write-out "%{http_code}\n")
+
+echo $response
