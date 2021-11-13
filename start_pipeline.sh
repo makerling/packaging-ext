3092b12f1b3d2f5931fce6cb5ced1011d1f6fa8a
Stevan Vanderwerf <github@vanderling.com>
2021-11-13 11:44:07 -0600

    cleanup syntax

--- start_pipeline.sh
+++ start_pipeline.sh
@@ -9,4 +9,16 @@ response=$(curl -H "Accept: application/vnd.github.everest-preview+json" \
     https://api.github.com/repos/makerling/packaging-ext/dispatches \
     --write-out "%{http_code}\n")
 
+echo
 echo $response
+echo
+
+if [ "$response" == 204 ]
+then
+    echo "github actions workflow has been triggered (204)"
+elif [ "$response" == 422 ]
+then
+    echo "validation failed, check your token settings (422)"
+else
+    echo -e "there was an issue with the request:\n"$response"\n"
+fi
