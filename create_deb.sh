2ca090dd70adb51abe6b2d6cc1883252b6be689e
Stevan Vanderwerf <github@vanderling.com>
2021-11-14 13:10:29 -0600

    adding rpm build step with fpm

--- create_deb.sh
+++ create_deb.sh
@@ -1,29 +1,30 @@
 #! /bin/bash
 
-# options: v1.4.1,v1.3.3,v1.3.2,v1.3.1,v1.3.0,v1.2.0,v1.1.3,v1.1.2,v1.1.1,v1.1.0,v1.0.2,v1.0.1,v1.0.0
-pg_jobmon_version='v1.4.1'
+# options: 1.4.1,1.3.3,1.3.2,1.3.1,1.3.0,1.2.0,1.1.3,1.1.2,1.1.1,1.1.0,1.0.2,1.0.1,1.0.0
+pg_jobmon_version='1.4.1'
 # options: 9.2,9.3,9.4,9.5,9.6,10,11,12,13,14
-pg_version='14'
+pg_version='13'
 deb_rpm_version='1.0.0'
 
 # download and unpack source of the postgres extension
-wget https://github.com/omniti-labs/pg_jobmon/archive/refs/tags/"$pg_jobmon_version".tar.gz
+wget https://github.com/omniti-labs/pg_jobmon/archive/refs/tags/v"$pg_jobmon_version".tar.gz
 filename=$(ls *.tar.gz)
 tar xvzf "$filename"
 folder=$(ls -d */)
 foldername=${folder:0:-1}
 
-# install postgres and build tools
-sudo apt install curl ca-certificates gnupg
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-sudo apt update && sudo apt -y install postgresql-"$pg_version"
-sudo apt update && sudo apt install build-essential libicu-dev \
-    postgresql-server-dev-"$pg_version" -y --no-install-recommends
+# # install postgres and build tools
+# sudo apt install curl ca-certificates gnupg
+# curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# sudo apt update && sudo apt -y install postgresql-"$pg_version"
+# sudo apt update && sudo apt install build-essential libicu-dev \
+#     postgresql-server-dev-"$pg_version" -y --no-install-recommends
 
 # build extension from source
-cd "$foldername"            
-make && make install
+cd "$foldername"
+# mkdir install
+make && make install DESTDIR=./install
 
 # package artifact as deb
 mkdir ./DEBIAN/
@@ -42,4 +43,16 @@ Description: pg_jobmon is a PostgreSQL extension designed to add autonomous \n\
 
 cd .. 
 sudo dpkg-deb --build "$foldername"
+
+echo "starting rpm packaging"
+
+# sudo apt-get update && sudo apt-get install ruby ruby-dev rubygems gcc make rpm
+# sudo gem install --no-document fpm
+fpm --input-type dir --output-type rpm  \
+    --version "$pg_jobmon_version" \
+    --package pg_jobmon-"$pg_jobmon_version".rpm \
+    --depends "rh-postgresql"$pg_version"-postgresql" \
+    --name pg_jobmon-"$pg_jobmon_version" \
+    "$foldername"/install
+
 echo "finished successfully"
